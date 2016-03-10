874fa1f27da5e4bbfc3da16142935c78722d155c
İsmail Başaran <basaran.ismaill@gmail.com>
2016-03-10 14:24:50 +0200

    initial commit

---
+++ lider-console-lider-ahenk-browser-plugin/src/tr/org/liderahenk/browser/plugin/editors/SampleProfileEditor.java
@@ -0,0 +1,78 @@
+package tr.org.liderahenk.browser.plugin.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class SampleProfileEditor extends EditorPart {
+	
+	public SampleProfileEditor() {
+		super();
+	}
+	
+	@Override
+	public void createPartControl(final Composite parent) {
+		/*
+		 * Auto-generated.
+		 */
+	}
+	
+	/**
+	 * @see org.eclipse.ui.part.EditorPart#doSave(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override	
+	public void doSave(IProgressMonitor monitor) {
+		/*
+		 * Auto-generated.
+		 */
+	}
+
+	/**
+	 * @see org.eclipse.ui.part.EditorPart#doSaveAs()
+	 */
+	@Override	
+	public void doSaveAs() {
+		/*
+		 * Auto-generated.
+		 */
+	}
+
+	/**
+	 * @see org.eclipse.ui.part.EditorPart#init(org.eclipse.ui.IEditorSite, org.eclipse.ui.IEditorInput)
+	 */
+	@Override	
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		setSite(site);
+		setInput(input);
+		setPartName(input.getName());
+	}
+	
+	@Override 
+	public void dispose() {
+		/*
+		 * Auto-generated.
+		 */
+	}
+	
+	@Override
+	public boolean isDirty() {
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+	
+	@Override
+	public void setFocus() {
+		/*
+		 * Auto-generated.
+		 */	
+	}
+	
+}
---
+++ lider-lider-ahenk-browser-plugin/src/main/java/tr/org/liderahenk/browser/plugin/commands/BaseCommand.java
@@ -0,0 +1,20 @@
+package tr.org.liderahenk.browser.plugin.commands;
+
+import tr.org.liderahenk.lider.core.api.plugin.ICommand;
+
+public abstract class BaseCommand implements ICommand {
+
+	private static final String NAME = "lider-lider-ahenk-browser-plugin";
+	private static final String VERSION = "1.0.0";
+
+	@Override
+	public String getPluginName() {
+		return NAME;
+	}
+
+	@Override
+	public String getPluginVersion() {
+		return VERSION;
+	}
+
+}
