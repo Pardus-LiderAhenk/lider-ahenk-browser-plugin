93c94e28bc24645239769082110fa817fe29603f
Volkan Şahin <volkan.sahin@agem.com.tr>
2016-05-09 16:49:55 +0300

    safe.py added for safe mode

---
+++ ahenk-browser/browser/safe.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: Volkan Şahin <volkansah.in> <bm.volkansahin@gmail.com>
+
+
+def handle_safe_mode(username,context):
+    print('browser safe mode')
+    pass
