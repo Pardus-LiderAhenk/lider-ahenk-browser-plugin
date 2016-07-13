81973bbe7d123ea1822216f9497f6c1b6f655e32
Volkan Şahin <volkan.sahin@agem.com.tr>
2016-07-13 17:02:57 +0300

    adapted new modes

--- ahenk-browser/browser/safe.py
+++ ahenk-browser/browser/safe.py
@@ -2,7 +2,21 @@
 # -*- coding: utf-8 -*-
 # Author: Volkan Şahin <volkansah.in> <bm.volkansahin@gmail.com>
 
+from base.plugin.abstract_plugin import AbstractPlugin
 
-def handle_safe_mode(username,context):
-    print('browser safe mode')
-    pass
+
+class Safe(AbstractPlugin):
+    def __init__(self, context):
+        super(Safe, self).__init__()
+        self.context = context
+        self.name = str(context.get_username())
+        self.logger = self.get_logger()
+
+    def handle_safe_mode(self):
+        # TODO Do what do you want to do!
+        pass
+
+
+def handle_mode(context):
+    sample = Safe(context)
+    sample.handle_safe_mode()
