f3bc5d4797e62a33aa34dfb87048d43c5f768493
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-05 17:43:55 +0300

    log fixing

--- ahenk-browser/browser/policy.py
+++ ahenk-browser/browser/policy.py
@@ -20,24 +20,24 @@ class Browser(AbstractPlugin):
         self.mozilla_config_file = 'mozilla.cfg'
         self.local_settings_JS_file = 'local-settings.js'
         self.local_settings_JS_path = 'defaults/pref/'
-        self.logger.info('[Browser] Parameters were initialized.')
+        self.logger.info('Parameters were initialized.')
 
     def handle_policy(self):
-        self.logger.info('[Browser] Browser plugin handling...')
+        self.logger.info('Browser plugin handling...')
         try:
             username = self.context.get('username')
-            self.logger.info('[Browser] Username: {}'.format(username))
+            self.logger.info('Username: {}'.format(username))
             if username is not None:
-                self.logger.debug('[Browser] Writing preferences to user profile')
+                self.logger.debug('Writing preferences to user profile')
                 self.write_to_user_profile(username)
                 self.context.create_response(code=self.message_code.POLICY_PROCESSED.value, message='Kullanıcı browser profili başarıyla uygulandı.')
             else:
-                self.logger.debug('[Browser] Writing preferences to global profile')
+                self.logger.debug('Writing preferences to global profile')
                 self.write_to_global_profile()
                 self.context.create_response(code=self.message_code.POLICY_PROCESSED.value, message='Ajan browser profili başarıyla uygulandı.')
-            self.logger.info('[Browser] Browser profile is handled successfully')
+            self.logger.info('Browser profile is handled successfully')
         except Exception as e:
-            self.logger.error('[Browser] A problem occurred while handling browser profile: {0}'.format(str(e)))
+            self.logger.error('A problem occurred while handling browser profile: {0}'.format(str(e)))
             self.context.create_response(code=self.message_code.POLICY_ERROR.value, message='Browser profili uygulanırken bir hata oluştu.')
 
     def write_to_user_profile(self, username):
@@ -51,7 +51,7 @@ class Browser(AbstractPlugin):
                 path = str(path) + '/user.js'
                 user_jss = open(path, 'w')
                 preferences = json.loads(self.data)['preferences']
-                self.logger.debug('[Browser] Writing preferences to user.js file ...')
+                self.logger.debug('Writing preferences to user.js file ...')
                 for pref in preferences:
                     if pref['value'].isdigit() or str(pref['value']) == 'false' or str(pref['value']) == 'true':
                         value = pref['value']
@@ -60,22 +60,22 @@ class Browser(AbstractPlugin):
                     line = 'user_pref("' + str(pref['preferenceName']) + '",' + value + ');\n'
                     user_jss.write(line)
 
-                self.logger.debug('[Browser] User preferences were wrote successfully')
+                self.logger.debug('User preferences were wrote successfully')
                 user_jss.close()
                 change_owner = 'chown ' + username + ':' + username + ' ' + path
                 self.execute(change_owner)
-                self.logger.debug('[Browser] Preferences file owner is changed')
+                self.logger.debug('Preferences file owner is changed')
 
         except Exception as e:
-            self.logger.error('[Browser] A problem occurred while writing user profile: {0}'.format(str(e)))
+            self.logger.error('A problem occurred while writing user profile: {0}'.format(str(e)))
             # Remove global lock files to tell Firefox to load the user file
         installation_path = self.find_firefox_installation_path()
         if installation_path is None:
-            self.logger.error('[Browser] Firefox installation directory could not be found! Finishing task...')
+            self.logger.error('Firefox installation directory could not be found! Finishing task...')
             return
         self.silent_remove(str(installation_path) + self.mozilla_config_file)
         self.silent_remove(str(installation_path) + self.local_settings_JS_path + self.local_settings_JS_file)
-        self.logger.debug('[Browser] User profiles have been set successfully')
+        self.logger.debug('User profiles have been set successfully')
 
     def write_to_global_profile(self):
         firefox_installation_path = self.find_firefox_installation_path()
@@ -83,10 +83,10 @@ class Browser(AbstractPlugin):
         try:
             preferences = json.loads(str(self.data))['preferences']
         except Exception as e:
-            self.logger.error('[Browser] Problem occurred while getting preferences. Error Message: {}'.format(str(e)))
+            self.logger.error('Problem occurred while getting preferences. Error Message: {}'.format(str(e)))
 
         mozilla_cfg = open(str(firefox_installation_path) + self.mozilla_config_file, 'w')
-        self.logger.debug('[Browser] Mozilla configuration file is created')
+        self.logger.debug('Mozilla configuration file is created')
         for pref in preferences:
             if pref['value'].isdigit() or str(pref['value']) == 'false' or str(pref['value']) == 'true':
                 value = pref['value']
@@ -95,27 +95,27 @@ class Browser(AbstractPlugin):
             line = 'lockPref("' + str(pref['preferenceName']) + '",' + value + ');\n'
             mozilla_cfg.write(line)
         mozilla_cfg.close()
-        self.logger.debug('[Browser] Preferences were wrote to Mozilla configuration file')
+        self.logger.debug('Preferences were wrote to Mozilla configuration file')
 
         local_settings_path = str(firefox_installation_path) + self.local_settings_JS_path
         if not self.is_exist(local_settings_path):
-            self.logger.debug('[Browser] Firefox local setting path not found, it will be created')
+            self.logger.debug('Firefox local setting path not found, it will be created')
             self.create_directory(local_settings_path)
         local_settings_js = open(local_settings_path + self.local_settings_JS_file, 'w')
         local_settings_js.write(
                 'pref("general.config.obscure_value", 0);\npref("general.config.filename", "mozilla.cfg");\n')
         local_settings_js.close()
-        self.logger.debug('[Browser] Firefox local settings were configured')
+        self.logger.debug('Firefox local settings were configured')
 
     def silent_remove(self, filename):
         try:
             if self.is_exist(filename):
                 self.delete_file(filename)
-                self.logger.debug('[Browser] {0} removed successfully'.format(filename))
+                self.logger.debug('{0} removed successfully'.format(filename))
             else:
-                self.logger.warning('[Browser] {0} was tried to delete but not found.'.format(filename))
+                self.logger.warning('{0} was tried to delete but not found.'.format(filename))
         except Exception as e:
-            self.logger.error('[Browser] Problem occurred while removing file {0}. Exception Message is: {1}'.format(filename, str(e)))
+            self.logger.error('Problem occurred while removing file {0}. Exception Message is: {1}'.format(filename, str(e)))
 
     def find_user_preference_paths(self, user_name):
 
@@ -127,10 +127,10 @@ class Browser(AbstractPlugin):
             if 'Path' in line:
                 paths.append(firefox_path + str(line.split('=')[1]).strip())
         if len(paths) > 0:
-            self.logger.debug('[Browser] User preferences path found successfully')
+            self.logger.debug('User preferences path found successfully')
             return paths
         else:
-            self.logger.error('[Browser] User preferences path not found')
+            self.logger.error('User preferences path not found')
 
     def find_firefox_installation_path(self):
         installation_path = '/usr/lib/firefox/'
@@ -139,9 +139,9 @@ class Browser(AbstractPlugin):
         if not self.is_exist(installation_path):
             installation_path = '/usr/lib/iceweasel/'
         if not self.is_exist(installation_path):
-            self.logger.error('[Browser] Firefox installation path not found')
+            self.logger.error('Firefox installation path not found')
             return None
-        self.logger.debug('[Browser] Firefox installation path found successfully')
+        self.logger.debug('Firefox installation path found successfully')
         return installation_path
 
 
