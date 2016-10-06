a0909d885d542fe05c73d306395684474a9aa6c5
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-06 16:43:09 +0300

    main module info added

--- ahenk-browser/browser/main.py
+++ ahenk-browser/browser/main.py
@@ -10,6 +10,9 @@ def info():
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
     inf['description'] = 'Browser plugin provides to managing user or global firefox profile like setting homepage etc.'
+    inf['task'] = True
+    inf['user_oriented'] = True
+    inf['machine_oriented'] = True
     inf['developer'] = 'bm.volkansahin@gmail.com'
 
     return inf
