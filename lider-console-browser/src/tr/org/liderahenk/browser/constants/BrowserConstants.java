a36191a1b21839db291812838b6b4c3d5a10791b
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-25 15:49:44 +0300

    fix 1.0.0-beta version

--- lider-console-browser/src/tr/org/liderahenk/browser/constants/BrowserConstants.java
+++ lider-console-browser/src/tr/org/liderahenk/browser/constants/BrowserConstants.java
@@ -4,7 +4,7 @@ public class BrowserConstants {
 
 	public static final String PLUGIN_NAME = "browser";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 	
 	public static final String PREFERENCES_MAP_KEY = "preferences";
 
